0c531a6b5d4aebc1d430ec8291a2c9988f278229
Ting Chun Liu <8765498@gmail.com>
2023-07-13 12:02:59 +0200

    add new upload

--- RN50Clip.py
+++ RN50Clip.py
@@ -4,23 +4,18 @@ import torchvision.transforms as transforms
 from PIL import Image
 from torchvision.models import resnet50
 from torch.nn import functional as F
-from clip import clip
+from transformers import CLIPProcessor, CLIPModel
 
-# Load the CLIP model
+# Load the CLIP model and processor
 device = "cuda" if torch.cuda.is_available() else "cpu"
-model, preprocess = clip.load("RN50", device=device)
+model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32").to(device)
+processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 
-# Load the ResNet50 model for image preprocessing
-resnet_model = resnet50(pretrained=True).to(device)
-resnet_model.eval()
-preprocess_resnet = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-])
+# Define the target classes
+target_classes = ["happy", "sad"]
 
-# Define the target class
-target_class = "happy"
+# Load the Haar cascade for face detection
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 
 # Start the camera
 cap = cv2.VideoCapture(0)
@@ -29,37 +24,60 @@ while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
 
-    # Convert the OpenCV frame to a PIL image
-    image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+    # Convert the frame to grayscale for face detection
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Preprocess the image for CLIP
-    image_preprocessed = preprocess(image).unsqueeze(0).to(device)
+    # Detect faces in the frame
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
-    # Preprocess the image for ResNet50
-    image_resnet = preprocess_resnet(image).unsqueeze(0).to(device)
+    max_face_area = 0
+    max_face_img = None
 
-    # Run the image through ResNet50
-    with torch.no_grad():
-        features = resnet_model(image_resnet)
-        features = F.normalize(features, dim=1)
+    for (x, y, w, h) in faces:
+        # Select the face region
+        face_img = frame[y:y+h, x:x+w]
 
-    # Generate text features from the image using CLIP
-    with torch.no_grad():
-        image_features = model.encode_image(image_preprocessed)
-        image_features = F.normalize(image_features, dim=1)
+        # Calculate the face area
+        face_area = w * h
 
-        text = clip.tokenize([target_class]).to(device)
-        text_features = model.encode_text(text)
-        text_features = F.normalize(text_features, dim=1)
+        if face_area > max_face_area:
+            max_face_area = face_area
+            max_face_img = face_img
 
-        # Calculate the similarity score between the image and the target class
-        similarity = (100.0 * image_features @ text_features.T).softmax(dim=-1)
+    if max_face_img is not None:
+        # Convert the OpenCV face image to a PIL image
+        face_pil = Image.fromarray(cv2.cvtColor(max_face_img, cv2.COLOR_BGR2RGB))
 
-    # Check if the person is happy based on the similarity score
-    is_happy = similarity.item() > 0.5
+        # Preprocess the face image using the CLIP processor
+        inputs = processor(
+            images=face_pil,
+            texts=["dummy text"],
+            return_tensors="pt",
+            padding=True,
+            truncation=True
+        )
 
-    # Display the result on the frame
-    cv2.putText(frame, f"Happy: {is_happy}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        # Forward pass through the CLIP model
+        with torch.no_grad():
+            inputs = {k: v.to(device) for k, v in inputs.items()}
+            outputs = model(**inputs)
+            logits_per_image = outputs.logits_per_image  # This is the image-text similarity score
+            probs = logits_per_image.softmax(dim=1)  # Softmax to get the label probabilities
+
+        # Determine the predicted emotion label
+        max_prob, max_idx = probs[0].max(dim=0)
+        predicted_emotion = target_classes[max_idx.item()]
+        similarity_score = max_prob.item()
+
+        # Draw rectangle around the face
+        (x, y, w, h) = faces[0]
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+        # Display the predicted emotion and similarity score
+        emotion_label = f"{predicted_emotion.capitalize()} ({similarity_score:.2f})"
+        cv2.putText(frame, emotion_label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+
+    # Display the frame
     cv2.imshow('Camera', frame)
 
     # Exit the loop if 'q' is pressed
--- happiness.py
+++ happiness.py
@@ -19,3 +19,6 @@ inputs = processor(text=["a photo of a cat", "a photo of a dog"], images=image, 
 outputs = model(**inputs)
 logits_per_image = outputs.logits_per_image # this is the image-text similarity score
 probs = logits_per_image.softmax(dim=1) # we can take the softmax to get the label probabilities
+
+print(outputs)
+print(probs)
